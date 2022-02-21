04c3f61c1abaf90f9b5db2dcf7aad1b7c9899b20
stee1e <97261406+stee1e@users.noreply.github.com>
2022-02-21 16:37:54 +0000

    further updates - js and darkmode

--- portfolio v1/animations.js
+++ portfolio v1/animations.js
@@ -18,7 +18,45 @@ function openAbout() {
  // document.getElementById("main").style.marginLeft = "100%";
 }
 
+function closeAbout() {
+  document.getElementById("aboutslider").style.width = "0%";
+ // document.getElementById("main").style.marginLeft = "100%";
+}
 
+/*Project slider*/
+function openProjects() {
+  document.getElementById("projectslider").style.width = "100%";
+ // document.getElementById("main").style.marginLeft = "100%";
+}
+
+function closeProjects() {
+  document.getElementById("projectslider").style.width = "0%";
+ // document.getElementById("main").style.marginLeft = "100%";
+}
+
+/*Contact slider*/
+function openContact() {
+  document.getElementById("contactslider").style.width = "100%";
+ // document.getElementById("main").style.marginLeft = "100%";
+}
+
+function closeContact() {
+  document.getElementById("contactslider").style.width = "0%";
+ // document.getElementById("main").style.marginLeft = "100%";
+}
+
+
+/*mouse follow*/
+var $container = $('.front-container');
+var $drone = $('.floating-text');
+var droneCenter = {
+  x: $drone.width() / 2,
+  y: $drone.height() / 2
+};
+
+$container.on('mousemove', function(event) {
+  $drone.css('transform', `translate3d(${event.offsetX - droneCenter.x}px, ${event.offsetY - droneCenter.y}px, 0)`);
+});
 
 
 /* Dark theme toggle*/
